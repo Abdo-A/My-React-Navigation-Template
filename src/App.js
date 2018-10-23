7d78af2c5a84486e5bf58088d105bbbe37bf8c26
Abdo <abdopoetry@gmail.com>
2018-10-23 20:18:11 +0200

    Removed unnecessary file App.js in src

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import BottomTabNavigator from "./src/BottomTabNavigator";
-
-export default class App extends Component {
-  render() {
-    return <BottomTabNavigator />;
-  }
-}
